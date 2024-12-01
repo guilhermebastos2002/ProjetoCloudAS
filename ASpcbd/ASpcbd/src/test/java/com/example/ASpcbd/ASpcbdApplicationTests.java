89b9a6c409f70ef0d73c716d1648a455e4f1bd57
Guilherme Bastos <126486967+guilhermebastos2002@users.noreply.github.com>
2024-11-30 21:58:02 -0300

    codigo certo

---
+++ ASpcbd/ASpcbd/src/test/java/com/example/ASpcbd/ASpcbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ASpcbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASpcbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
